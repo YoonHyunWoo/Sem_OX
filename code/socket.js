3af0eaa61648099e5adf0c2c2722d530f06be9dd
yoonhyunwoo <yjs88zerg@gmail.com>
2022-05-11 20:46:30 +0900

    allow cors

--- code/socket.js
+++ code/socket.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const server = app.listen(3000);
 const io = require('socket.io')(server);
+const cors = require('cors')
+app.use(cors())
 
 io.on('connection', (socket) => {
     //console.log("Client connected!");
