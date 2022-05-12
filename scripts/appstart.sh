07c08040281aa146a4a2988fb11bf358fe0843fb
YoonHyunWoo <yjs88zerg@gmail.com>
2022-05-12 11:51:27 +0900

    add aws ecr login code in appstart.sh

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 593634833876.dkr.ecr.ap-northeast-2.amazonaws.com
 docker run -p 3000:3000 -d 593634833876.dkr.ecr.ap-northeast-2.amazonaws.com/semox_repo:latest
