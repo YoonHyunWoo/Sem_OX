abceb8e143d237241783722228a6dfcd4c5dd559
yoonhyunwoo <yjs88zerg@gmail.com>
2022-05-12 20:30:40 +0900

    copy clipboard and CCU count

--- code/socket.js
+++ code/socket.js
@@ -8,10 +8,15 @@ app.use(cors())
 io.sockets.addListener('connection', (socket)=>{
     room = socket.handshake.query.room
     socket.join(room)
-    CCU = Object.keys(JSON.stringify(io.nsps['/'].adapter.rooms[room].sockets)).length
-    socket.emit('CCU', CCU)
+    setInterval(() => {
+        CCU = (Object.keys(JSON.stringify(io.nsps['/'].adapter.rooms[room].sockets)).length-1)/28    
+        socket.emit('CCU', CCU)
+    }, 5000);
+    
     
 })
+
+
 
 
 app.get('/health', (req,res)=>{
