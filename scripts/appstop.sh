ba617a0d00c644e7c5ef09f4cc7046f954100ba3
yoonhyunwoo <yjs88zerg@gmail.com>
2022-05-13 19:52:42 +0900

    cors error

--- scripts/appstop.sh
+++ scripts/appstop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker container rm -f $(docker container ls -aq) > /dev/null 2>&1
-docker image rm -f $(docker image ls -q) > /dev/null 2>&1
+docker container rm -f $(docker container ls -aq)
+docker image rm -f $(docker image ls -q)
+rm -rf /home/ec2-user/*
