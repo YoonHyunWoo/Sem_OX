50721f39dfdee581c66bf4bedc11c241f7611c02
yoonhyunwoo <yjs88zerg@gmail.com>
2022-05-16 19:41:06 +0900

    cors preflight

--- code/socket.ts
+++ code/socket.ts
@@ -3,8 +3,14 @@ const http = require('http')
 const server = http.createServer().listen(3000)
 const cors = require('cors')
 const io = require('socket.io')(server);
-app.use(cors())
-io.sockets.addListener('connection', (socket) => {
+
+
+const corsOpt = function(req, callbank) {
+    callbank(null, {origin: true});
+  };
+
+app.options('*', cors(corsOpt))
+  io.sockets.addListener('connection', (socket) => {
     let room = socket.handshake.query.room
     socket.join(room)
     let CCU: number = (Object.keys(JSON.stringify(io.nsps['/'].adapter.rooms[room].sockets)).length - 1) / 28
