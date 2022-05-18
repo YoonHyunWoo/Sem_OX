9498ece97a0516c41272286cc11f0d913d2830c1
yoonhyunwoo <yjs88zerg@gmail.com>
2022-05-18 21:22:56 +0900

    깨달아버린 나

--- code/socket.js
+++ code/socket.js
@@ -1,25 +1,19 @@
-const app = require('express')()
-const http = require('http')
-const server = app.listen(3000)
-const cors = require('cors')
-const io = require('socket.io')(server);
-app.use(cors())
-
-let CCU = 0;
-
-io.on('connection', (socket)=>{
-    CCU++;
-    socket.emit('CCU', CCU)
-})
-
-io.on('disconnected', (socket)=>{
-    CCU--;
-    socket.emit('CCU', CCU)
-})
-
-app.get('/health', (req, res) => {
+var app = require('express')();
+var http = require('http');
+var server = app.listen(3000);
+var cors = require('cors');
+var io = require('socket.io')(server);
+app.use(cors());
+io.on('connection', function (socket) {
+    socket.on('room', function (data) {
+        console.log(data);
+        socket.join(data);
+        var clients = io.sockets.clients();
+        console.log(clients);
+    });
+});
+app.get('/health', function (req, res) {
     res.send({
         "status": "ok"
-    })
-})
-
+    });
+});
---
+++ code/socket.ts
@@ -0,0 +1,32 @@
+const app = require('express')()
+const http = require('http')
+const server = app.listen(3000)
+const cors = require('cors')
+const io = require('socket.io')(server);
+app.use(cors())
+io.on('connection', (socket) => {
+
+    socket.on('room', (data)=>{
+        socket.join(data)
+        socket.room = data
+        io.socket.clients(socket.room)
+        
+        
+    })
+    socket.on('name', (data)=>{
+        socket.name = data
+    })
+
+    socket.on('disconnecting',()=>{
+        socket.leave(socket.room)
+    })
+        
+        
+})
+
+
+app.get('/health', (req,res) => {
+    res.send({
+        "status": "ok"
+    })
+})
