7b25ecaa6b2e677a1405ee7d3c0b995bb7b39a4b
yoonhyunwoo <yjs88zerg@gmail.com>
2022-05-19 21:41:50 +0900

    chating

--- code/socket.ts
+++ code/socket.ts
@@ -4,25 +4,27 @@ const server = app.listen(3000)
 const cors = require('cors')
 const io = require('socket.io')(server);
 app.use(cors())
-io.on('connection', (socket) => {
 
+
+
+io.on('connection', (socket) => {
     socket.on('room', (data)=>{
         socket.join(data)
         socket.room = data
-        io.to(socket.room).emit('user', socket.name)
     })
+
     socket.on('name', (data)=>{
         socket.name = data
+        io.to(socket.room).emit('message',socket.name + " 님이 접속하셨습니다")
+    })
+
+    socket.on('message', (data)=>{
+        socket.broadcast.to(socket.room).emit('message',data)
     })
 
     socket.on('disconnecting',()=>{
+        io.to(socket.room).emit('message',socket.name + " 님이 퇴장하셨습니다")
+        io.to(socket.room).emit('deleted User', socket.name)
         socket.leave(socket.room)
-    })
-})
-
-
-app.get('/health', (req,res) => {
-    res.send({
-        "status": "ok"
     })
 })
