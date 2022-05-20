91b4f3e0549b151e30af2d68934016c7bd9ae60c
yoonhyunwoo <yjs88zerg@gmail.com>
2022-05-20 21:38:11 +0900

    layout and joined user

--- code/socket.ts
+++ code/socket.ts
@@ -6,25 +6,39 @@ const io = require('socket.io')(server);
 app.use(cors())
 
 
-
+let rooms = {}
 io.on('connection', (socket) => {
+    
     socket.on('room', (data)=>{
         socket.join(data)
         socket.room = data
-    })
+        
+        
+})
 
     socket.on('name', (data)=>{
         socket.name = data
         io.to(socket.room).emit('message',socket.name + " 님이 접속하셨습니다")
+        if(rooms[socket.room] == undefined){
+            rooms[socket.room] = socket.name + ' '
+        }else{
+            rooms[socket.room] += socket.name + ' '
+        }
+        io.to(socket.room).emit('userCount',rooms[socket.room])
+        
+        
     })
 
     socket.on('message', (data)=>{
-        socket.broadcast.to(socket.room).emit('message',data)
+        socket.broadcast.to(socket.room).emit('message',socket.name + ' : ' + data)
     })
 
     socket.on('disconnecting',()=>{
         io.to(socket.room).emit('message',socket.name + " 님이 퇴장하셨습니다")
-        io.to(socket.room).emit('deleted User', socket.name)
+        rooms[socket.room] = rooms[socket.room].replace(socket.name + ' ', '')
+        io.to(socket.room).emit('userCount',rooms[socket.room])
         socket.leave(socket.room)
     })
+    
 })
+
